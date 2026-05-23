f233d81cf95d6ce55fcbb5d1e5b4c881ce63742f
ayushanand <ayushanandtrp@gmail.com>
2026-05-23 20:43:38 +0530

    Added Look and Say solution

--- src/Recursion/LookAndSay.java
+++ src/Recursion/LookAndSay.java
@@ -2,25 +2,28 @@ package Recursion;
 
 public class LookAndSay {
     public static void main(String[]args){
-
+        System.out.println(Pattern(3));
     }
-    public static void Pattern(int n){
+    public static  String Pattern(int n){
         if(n == 1) return "1";
         String s = Pattern(n-1);
         String ans ="";
         int i = 0 ;
         int j = 0;
-        while(i < s.length()){
+        while(j < s.length()){
             if(s.charAt(i) == s.charAt(j)){
                 j++;
             }
             else{
-                int freq = i -j;
+                int freq = j-i;
                 ans += freq;
-                ans +=i;
-
+                ans +=s.charAt(i);
+                i = j;
             }
         }
-
+        int freq = j-i;
+        ans += freq;
+        ans +=s.charAt(i);
+        return ans;
     }
 }
